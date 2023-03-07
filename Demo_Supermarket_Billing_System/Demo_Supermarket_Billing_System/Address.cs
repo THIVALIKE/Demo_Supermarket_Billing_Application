a886cbff150358e11b61387e8c42f0b58550590a
THIVALIKE <thivajm007@gmail.com>
2023-03-07 15:47:26 +0530

    Customer class && Address class completed

---
+++ Demo_Supermarket_Billing_System/Demo_Supermarket_Billing_System/Address.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_Supermarket_Billing_System
+{
+    public class Address
+    {
+        string _house_no;
+
+        public string House_no
+        {
+            get { return _house_no; }
+        }
+        string _street_name;
+
+        public string Street_name
+        {
+            get { return _street_name; }
+        }
+        string _village_name;
+
+        public string Village_name
+        {
+            get { return _village_name; }
+        }
+        string _taluk_name;
+
+        public string Taluk_name
+        {
+            get { return _taluk_name; }
+        }
+        string _pincode;
+
+        public string Pincode
+        {
+            get { return _pincode; }
+        }
+        string _district;
+
+        public string District
+        {
+            get { return _district; }
+        }
+
+        public Address(string house_no, string street_name, string village_name, string taluk_name, string district, string pin_code)
+        {
+            this._house_no = house_no;
+            this._street_name = street_name;
+            this._village_name = village_name;
+            this._taluk_name = taluk_name;
+            this._district = district;
+            this._pincode = pin_code;
+        }
+
+        public Address()
+        {
+
+        }
+    }
+}
---
+++ Demo_Supermarket_Billing_System/Demo_Supermarket_Billing_System/Customer.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_Supermarket_Billing_System
+{
+    public class Customer
+    {
+        int _id;
+
+        public int Id
+        {
+            get { return _id; }
+        }
+
+        string _name;
+
+        public string Name
+        {
+            get { return _name; }
+
+        }
+
+        gender gender;
+
+        public gender Gender
+        {
+            get { return gender; }
+            
+        }
+        string _mobile_num;
+
+        public string Mobile_num
+        {
+            get { return _mobile_num; }
+
+        }
+
+        string _email_id;
+
+        public string Email_id
+        {
+            get { return _email_id; }
+
+        }
+
+        Address address_details;
+
+        public Address Address_details
+        {
+            get { return address_details; }
+            set { address_details = value; }
+        }
+
+        public Customer(int Id, string Name,gender Gen ,string Mobile_num, string Email_id, Address Address_info)
+        {
+            this._id = Id;
+            this._name = Name;
+            this.gender = Gen;
+            this._mobile_num = Mobile_num;
+            this._email_id = Email_id;
+            this.Address_details = Address_info;
+        }
+
+        public Customer()
+        {
+
+        }
+
+        
+    }
+
+    enum gender
+    {
+        Male=0,
+        Female=1,
+        Trans=2
+    }
+}
